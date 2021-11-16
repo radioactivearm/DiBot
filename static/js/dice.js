9ef35814cac037e638642fdfcb1c5f75f7a8912e
arm <andy.mcrae21@gmail.com>
2021-11-15 23:14:50 -0600

    Got Button to work

--- static/js/dice.js
+++ static/js/dice.js
@@ -2,14 +2,14 @@ console.log('static/js/dice.js loaded');
 
 function ShatterDice(rolls) {
     let axe = rolls.split('+');
-    console.log(axe);
+
     const undesirables = [' ', '!r'];
     for (i = 0; i < undesirables.length; i++) {
         for (j = 0; j < axe.length; j++) {
             axe[j] = axe[j].replace(undesirables[i], '');
         }
     }
-    console.log(axe);
+
     return axe
 }
 
@@ -24,14 +24,15 @@ function RollDice(dice) {
     }
     roll = mark[0] * Math.ceil(Math.random() * mark[1]);
 
-    console.log(roll);
+
     return roll;
 }
 
 function CollectRoll() {
-    let spread = d3.select('#roll').text;
+    let spread = document.getElementById('roll').value; 
+
     let axedSpread = ShatterDice(spread);
     console.log(axedSpread);
 }
 
-d3.select('.enter').on('click', CollectRoll());
+
