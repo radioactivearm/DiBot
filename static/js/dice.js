ae85c542bdd52981112c170c0f3b96298ca7b6df
arm <andy.mcrae21@gmail.com>
2021-11-16 12:33:02 -0600

    got the di roller working.

--- static/js/dice.js
+++ static/js/dice.js
@@ -1,5 +1,8 @@
 console.log('static/js/dice.js loaded');
 
+
+// Splits Roll input on plus sign, into individual di
+// dropping possible !r/!roll that one may put infront
 function ShatterDice(rolls) {
     let axe = rolls.split('+');
 
@@ -13,26 +16,70 @@ function ShatterDice(rolls) {
     return axe
 }
 
+// this one rolls the dice if it has a d/D in the middle
 function RollDice(dice) {
 
     let mark;
 
     if (dice.includes('d')) {
         mark = dice.split('d');
-    } else if (dice.inclues('D')) {
+        roll = mark[0] * Math.ceil(Math.random() * mark[1]);
+    } else if (dice.includes('D')) {
         mark = dice.split('D');
+        roll = mark[0] * Math.ceil(Math.random() * mark[1]);
+    } else {
+        roll = dice;
     }
-    roll = mark[0] * Math.ceil(Math.random() * mark[1]);
-
-
+    
     return roll;
 }
 
+// This puts the return on the page
+function PutReturn(added, unadded) {
+    document.getElementById('addedResult').innerHTML = added;
+    document.getElementById('unaddedResult').innerHTML = unadded;
+
+  
+}
+
+// This rejoins the rolled dice out puts so you can see
+// each di's out put
+function Rejoin(array) {
+    let jarray = array.join('+');
+    return jarray;
+
+}
+
+
+// this adds up all the dies out puts
+function AddArray(array) {
+
+    const reducer = (a, b) => parseInt(a) + parseInt(b);
+    return array.reduce(reducer);
+}
+
+
+// this runs everything
 function CollectRoll() {
+    // get value of roll
     let spread = document.getElementById('roll').value; 
 
+    // seperate value
     let axedSpread = ShatterDice(spread);
-    console.log(axedSpread);
+
+    let roll = [];
+
+    const len = axedSpread.length;
+    // roll dice
+    for (i = 0; i < len; i++) {
+        roll.push(RollDice(axedSpread[i]));
+    }
+
+    const added = AddArray(roll);
+    const unadded = Rejoin(roll);
+
+    PutReturn(added, unadded);
+    
 }
 
 
