fcc0347b1bba45b05e858f65bc835e4fb6286aae
arm <andy.mcrae21@gmail.com>
2021-11-16 19:17:24 -0600

    working on advantage rolls

--- static/js/dice.js
+++ static/js/dice.js
@@ -23,16 +23,48 @@ function RollDice(dice) {
 
     if (dice.includes('d')) {
         mark = dice.split('d');
-        roll = mark[0] * Math.ceil(Math.random() * mark[1]);
+        roll = 0;
+        for (m = 0; m < mark[0]; m++) {
+            roll = roll + Math.ceil(Math.random() * mark[1]);
+        }
     } else if (dice.includes('D')) {
         mark = dice.split('D');
-        roll = mark[0] * Math.ceil(Math.random() * mark[1]);
+        roll = 0;
+        for (m = 0; m < mark[0]; m++) {
+            roll = roll + Math.ceil(Math.random() * mark[1]);
+        }
     } else {
         roll = dice;
     }
     
     return roll;
 }
+
+
+//========================================
+
+function AdvRoll(diceArray, adv = true) {
+    let rolls = [];
+
+    diceArray.forEach(dice => {
+        let mark;
+        if (dice.includes('d')) {
+            mark = dice.split('d');
+            for (m = 0; m < mark[0]; m++) {
+                rolls.push(String(Math.ceil(Math.random() * mark[1])) + 'i');
+                rolls.push(String(Math.ceil(Math.random() * mark[1])) + 'j');
+            }
+        } else {
+            rolls.push(String(dice) + 'k');
+        }
+    });
+
+    return rolls;
+}
+
+//========================================
+
+
 
 // This puts the return on the page
 function PutReturn(added, unadded) {
@@ -58,7 +90,29 @@ function AddArray(array) {
     return array.reduce(reducer);
 }
 
+function TakeAdvantage(array) {
+    
+    for (i = 0; i < array.length - 1; i++) {
+        let diceOne;
+        let diceTwo;
+        if (array[i].includes('i') && array[i + 1].includes('j')) {
+            diceOne = array[i].replace('i', '');
+            diceTwo = array[i + 1].replace('j', '');
+            if (parseInt(diceOne) > parseInt(diceTwo)) {
+                array[i] = diceOne + 'h';
+                array[i + 1] = diceTwo + 'l';
+            } else {
+                array[i] = diceOne + 'l';
+                array[i + 1] = diceTwo + 'h';
+            }
+        }
 
+    }
+
+    return array;
+}
+
+//=========================================
 // this runs everything
 function CollectRoll() {
     // get value of roll
@@ -81,5 +135,22 @@ function CollectRoll() {
     PutReturn(added, unadded);
     
 }
+//============================================
 
+// advantage
+
+function Advantage() {
+    let spread = document.getElementById('roll').value;
+
+    let axedSpread = ShatterDice(spread);
+    console.log(axedSpread);
+    let adv = AdvRoll(axedSpread, true);
+
+    console.log(adv);
+
+    let newAdv = TakeAdvantage(adv);
+
+    console.log(newAdv);
+
+}
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,42 @@
 console.log('static/app.js');
 
-//var randy = Math.ceil(Math.random() * 20);
 
-//console.log(randy);
+// =====================================
+
+function Twenty() {
+/*    let dTwenty = document.getElementById('d20');*/
+
+    let input = document.getElementById('roll');
+
+    const currentRoll = input.value
+
+    let newRoll = '';
+
+    let diceArray = ShatterDice(currentRoll);
+    const len = diceArray.length;
+
+    if (diceArray[len - 1].includes('d20')) {
+        let timArray = diceArray[len - 1].split('d20');
+
+        let tim = timArray[0];
+
+        let plusTime = parseInt(tim) + 1;
+
+        diceArray[len - 1] = plusTime + 'd20';
+
+        newRoll = Rejoin(diceArray);
+    } else if (currentRoll == '') {
+        newRoll = '1d20';
+    } else {
+        newRoll = currentRoll + '+1d20';
+    }
 
 
-//function rTwenty() {
-//    let randy = Math.ceil(Math.random() * 20);
-//    return randy;
-//}
 
-//function printrTwenty() {
-//    console.log(rTwenty());
-//}
+    console.log(newRoll);
 
-//function insertRoll() {
-//    d3.select('#result').text('Roll here');
-//}
+    input.value = newRoll;
+}
 
-//let btn = d3.select(".r20");
 
-//btn.on('click', printrTwenty);
-
-//d3.select(".enter").on('click', insertRoll);
-
+//==================================================
